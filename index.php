b1bb6e29506470dae52783213097d100d41bb032
ThomasDias417 <94170854+ThomasDias417@users.noreply.github.com>
2022-05-15 19:00:19 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,51 @@
-<html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <title>RingRingRun</title>
-  </head>
-  <body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="style-index.css">
+  <title>Last Call the game</title>
+</head>
+<body>
 
-  <?php
-if(isset($_SESSION['membre'])) {
- ?>   
-    <a href="?action=deconnexion">Déconnexion</a>
-    <br>
-    <a href="home.php"></a>
+<?php
+    include('init.php');
+    if(isset($_SESSION['membre'])) {
+  ?>
+    <section class="outGameStream">
+      <div class="containerTransUp">
+          <div class="imgTrsUp">
+            <img src="image/bob1.png">
+          </div> 
+      </div>
 
+      <div class="containerChoix">
+        <a class="main-btn" href="?action=deconnexion">Déconnexion</a>
+        <a class="main-btn" href="home.php">Revenir au jeu</a>
+      </div>
+
+      <div class="containerTransDown">
+          <div class="imgTrsDown">
+            <img src="image/bob2.png">
+          </div> 
+      </div>
+    </section>
 <?php
  }   else {
 ?>
-    <div class="index-link" >
+  <section class = "onGameStream">
+    <h1>Last Call <span>the game</span></h1>
+
+    <div class="container-link" >
         <a class="main-btn" href="register.php">Inscription</a>
         <a class="main-btn" href="login.php">Connexion</a>
     </div>
+    <div class="bobPose1"><img src="image/bob1.png" alt="bob fais la pose"></div>
+    <div class="bobPose2"><img src="image/bob2.png" alt="bob fais la pose"></div>
+  </section>
+    
 <?php
          }
 ?>
--- settings.php
+++ settings.php
@@ -4,79 +4,72 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="style_test.css" rel="stylesheet">
-        <title>options</title>
+        <link  rel="stylesheet" href="reset.css">
+        <link  rel="stylesheet" href="style-setting.css">
+        <title>controle</title>
     </head>
 <?php
-    include "init.php";
-// si la session membre existe, alors je redirige vers l'accueil 
-    // if(isset($_SESSION['user'])){
-    // header('location:index.php');
-    // }
+    include ("init.php");
+    $pseudoMembreSetting = $_SESSION['membre']['pseudo'];
+    $RCP = $pdo -> query("SELECT droite FROM membre WHERE pseudo = '$pseudoMembreSetting' ");
+    $rightControlPlayer = $RCP->fetch(PDO::FETCH_ASSOC);
+
+    $LCP = $pdo -> query("SELECT gauche FROM membre WHERE pseudo = '$pseudoMembreSetting' ");
+    $leftControlPlayer = $LCP->fetch(PDO::FETCH_ASSOC);
+
+    $JCP = $pdo -> query("SELECT sauter FROM membre WHERE pseudo = '$pseudoMembreSetting' ");
+    $jumpControlPlayer = $JCP->fetch(PDO::FETCH_ASSOC);
+
+    $ACP = $pdo -> query("SELECT action FROM membre WHERE pseudo = '$pseudoMembreSetting' ");
+    $actionControlPlayer = $ACP->fetch(PDO::FETCH_ASSOC);
+
+    if (isset($_SESSION['membre']['pseudo'])) {
+        # code...
+    
 ?>
     <body>
         <main>
-            <section>
-                <h2 class="title-header">Options</h2>
-            <?php
-                $keyRight;
-                $keyLeft;
-                $keyUp;
+                <h2 class="title-header">Controle</h2>
+                <form method="get">
 
-                if($_POST){
-
-                    //PARTIE NON FINIE: récupérer un membre par l'ID dans la BDD pour modifier ses paramètres 
-
-                                //on part du principe qu'on a créé un utilisateur avec $_SESSION[idMembre]
-                                //$r = $pdo->query("SELECT * FROM membre WHERE idMembre = '$_SESSION[idMembre]'");
-                                //$membre = $r->fetch(PDO::FETCH_ASSOC);
-                            
-                                //html en php après en js
-
-                                //$idMembre = $_SESSION['idMembre'];
-                                //$recupMembre = $pdo->prepare("SELECT * FROM user WHERE idMembre=?");
-                                // // dans user, recup le membre qui a l'id correspondant dans la bdd 
-                                //$recupMembre->execute(array($idMembre));
-
-
-                    $keyRight = $_POST['optionR'];
-                    $keyLeft = $_POST['optionL'];
-                    $keyUp = $_POST['optionJ'];
-                     
-                    //$pdo->exec("INSERT INTO membre (droite) VALUES ('$_POST[optionR]')");
-                    //$pdo->exec("INSERT INTO membre (optionL) VALUES ('$_POST[optionL]')");
-                    //$pdo->exec("INSERT INTO membre (optionJ) VALUES ('$_POST[optionJ]')");
-
-                    //TEST pour passer des variables de php à JS        
-                    ?>
-                    <script>
-                        let jsvar = '<?=$keyRight?>';
-                        console.log(jsvar); 
-                    </script>
-            <?php } ?>
-
-                <form class="contForm" method ="post" name="optionForm"> 
-                    
-                        <div class="option">
-                            <label for="optionRight"> Touche pour se déplacemer à droite </label>
-                            <input type="text" name="optionR" required minlength="1" maxlength="1" size="3">
-                        </div>
-
-                        <div class="option">
-                            <label for="optionLeft">touche pour se déplacer à gauche </label>
-                            <input type="text" name="optionL" required minlength="1" maxlength="1" size="3">
-                        </div>
-
-                        <div class="option">
-                            <label for="optionJump">Touche pour sauter </label>
-                            <input type="text" name="optionJ" required minlength="1" maxlength="1" size="3">
-                        </div>
-
-                        <div class="option">
-                                <input class= "btnSubmit" type="submit" value="valider">
-                        </div>
-                </form>  
-            </section>
+                    <input type="text" name="droite" placeholder="Touche droite" required >
+                    <input type="text" name="gauche" placeholder="Touche gauche" required >
+                    <input type="text" name="sauter" placeholder="Touche sauter (si vous vouler assigné espace tapée : space) " required >
+                    <input type="text" name="action" placeholder="Touche action" required >
+                    <input type="submit" value = "appliquer">
+                </form>
+                <div class="actual">
+                    <h3>touches actuelle</h3>
+                    <ul>
+                        <li>droite :<?php echo $rightControlPlayer['droite']?></li>
+                        <li>gauche :<?php echo $leftControlPlayer['gauche']?><li>
+                        <li>sauter :<?php echo $jumpControlPlayer['sauter']?></li>
+                        <li>action :<?php echo $actionControlPlayer['action']?></li>
+                    </ul>
+                </div>
         </main>
     </body>
+    <?php
+
+$keyRight;
+$keyLeft;
+$keyUp;
+$keyAction;
+if($_GET){
+
+    $keyRight = $_GET['droite'];
+    $keyLeft = $_GET['gauche'];
+    $keyUp = $_GET['sauter'];
+    $keyAction = $_GET['action'];
+       
+    
+    $pdo->exec("UPDATE membre SET droite='$keyRight', gauche='$keyLeft', sauter='$keyUp', action='$keyAction' WHERE pseudo = '$pseudoMembreSetting' ");
+    header('Location: http://localhost/ringRingRun/home.php');
+}
+}
+else{
+    header('Location: http://localhost/ringRingRun/index.php');
+}
+?>
+
 </html>
